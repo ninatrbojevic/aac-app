d230be1c670afd7f0d2839888eafaf137923186e
Katarina <katarinatuhtan27@gmail.com>
2026-05-12 17:39:55 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import {provideAnimations} from '@angular/platform-browser/animations';
+import {bootstrapApplication} from '@angular/platform-browser';
+import {AppComponent} from './app/app.component';
+import {appConfig} from './app/app.config';
+import {providePrimeNG} from 'primeng/config';
+import Aura from '@primeng/themes/aura';
+import {provideHttpClient} from '@angular/common/http';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers ?? []),
+    provideAnimations(),
+    provideHttpClient(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
+  ]
+}).catch(err => console.error(err));
