8b75e37b4c3c6a2ebbaf161030aeede43549df8b
Katarina <katarinatuhtan27@gmail.com>
2026-06-04 12:56:58 +0200

    landing page set to login and auth guard added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,15 +3,20 @@ import { EventsComponent } from './events/events.component';
 import { UsersComponent } from './users/users.component';
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { RegistrationComponent} from './registration/registration.component';
+import { RegistrationComponent } from './registration/registration.component';
 import { DigitalContentComponent } from './digital-content/digital-content.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: HomepageComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegistrationComponent },
-  { path: 'users', component: UsersComponent },
-  { path: 'events', component: EventsComponent },
-  { path: 'digital-content', component: DigitalContentComponent },
+
+  { path: 'home', component: HomepageComponent, canActivate: [authGuard] },
+  { path: 'users', component: UsersComponent, canActivate: [authGuard] },
+  { path: 'events', component: EventsComponent, canActivate: [authGuard] },
+  { path: 'digital-content', component: DigitalContentComponent, canActivate: [authGuard] },
+
+  { path: '**', redirectTo: 'login' }
 ];
