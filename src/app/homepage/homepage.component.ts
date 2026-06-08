eab07ecea7f23243fcfc9adae91e4e107683e37b
Katarina <katarinatuhtan27@gmail.com>
2026-06-08 13:38:34 +0200

    requests counter for users and responsive design

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -9,6 +9,7 @@ import { EventService } from '../events/events.service';
 import { DigitalContentService } from '../digital-content/digital-content.service';
 import {AuthService} from '../shared/auth.service';
 import {UserRole} from '../user-role';
+import { DigitalContentRequestService } from '../digital-content-requests/digital-content-request.service';
 
 @Component({
   selector: 'app-homepage',
@@ -20,13 +21,15 @@ export class HomepageComponent implements OnInit {
   eventCount: number | null = null;
   userCount: number | null = null;
   contentCount: number | null = null;
+  requestCount: number | null = null;
 
   constructor(
     private router: Router,
     private authService: AuthService,
     private userService: UserService,
     private eventService: EventService,
-    private digitalContentService: DigitalContentService
+    private digitalContentService: DigitalContentService,
+    private requestService: DigitalContentRequestService
   ) { }
 
   isAdmin(): boolean {
@@ -35,8 +38,19 @@ export class HomepageComponent implements OnInit {
 
   ngOnInit() {
     this.eventService.getEvents().subscribe(events => this.eventCount = events.length);
-    this.userService.getUsers().subscribe(users => this.userCount = users.length);
     this.digitalContentService.getAll().subscribe(items => this.contentCount = items.length);
+  
+    if (this.isAdmin()) {
+      this.userService.getUsers().subscribe(users => this.userCount = users.length);
+      this.requestService.getAll().subscribe(requests => this.requestCount = requests.length);
+    } else {
+      const currentUser = this.authService.getCurrentUser();
+      if (currentUser) {
+        this.requestService.getByUser(currentUser._id).subscribe(
+          requests => this.requestCount = requests.length
+        );
+      }
+    }
   }
 
   goTo(route: string) {
